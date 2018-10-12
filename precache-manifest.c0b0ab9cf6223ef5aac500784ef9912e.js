83c7047e7aad9a915f07efb17278f3fe158d5226
Tannnnnn <tan_sweetmulet@hotmail.com>
2018-10-12 12:18:54 +0700

    Updates

---
+++ precache-manifest.c0b0ab9cf6223ef5aac500784ef9912e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SecondLight/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "908117c6b944bb737412",
+    "url": "/SecondLight/static/js/runtime~main.908117c6.js"
+  },
+  {
+    "revision": "316950a7a08bc5f1bdc5",
+    "url": "/SecondLight/static/js/main.316950a7.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/SecondLight/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "316950a7a08bc5f1bdc5",
+    "url": "/SecondLight/static/css/main.81f14a04.chunk.css"
+  },
+  {
+    "revision": "c38f3d01991c93bd66f5cca1bcedac0b",
+    "url": "/SecondLight/index.html"
+  }
+];
