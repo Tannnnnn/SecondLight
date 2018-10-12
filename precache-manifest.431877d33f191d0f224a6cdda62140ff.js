48e93e510edc2b465768bf90924ec4bc7718fbad
Tannnnnn <tan_sweetmulet@hotmail.com>
2018-10-12 12:25:38 +0700

    Updates

---
+++ precache-manifest.431877d33f191d0f224a6cdda62140ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1fbe48459b84e86c1a72",
+    "url": "/static/js/main.1fbe4845.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "1fbe48459b84e86c1a72",
+    "url": "/static/css/main.81f14a04.chunk.css"
+  },
+  {
+    "revision": "f18a79ce8a43b0b721b973fd32e413cd",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/secondlight/precache-manifest.80495ace66f80f3a27f4cf5ae7412bf1.js"
+  "/precache-manifest.431877d33f191d0f224a6cdda62140ff.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/secondlight/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
